436ff9aad8266fdc9d5147905cb23b2ce93e4419
Mirza Saram Khalil <158371398+MirzaSarim1@users.noreply.github.com>
2026-05-08 20:32:29 +0500

    Revert "feat: NestJS project setup - dependencies, configuration, and core structure"

--- src/common/decorators/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { ROLE_ENUM } from '../constants/constants';
-
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from './common/pipes/validation.pipe';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-    credentials: true,
-  });
-
-  app.useGlobalPipes(new ValidationPipe());
-
-  app.useGlobalFilters(new HttpExceptionFilter());
-
-  app.useGlobalInterceptors(new ResponseInterceptor());
-
-  const port = process.env.PORT || 3001;
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
